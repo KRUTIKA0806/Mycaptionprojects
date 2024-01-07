dbc192b191bad330e3c96f08ef7847c6694e2538
KRUTIKA0806 <155892112+KRUTIKA0806@users.noreply.github.com>
2024-01-07 20:01:53 +0530

    Added Setprogram.py file
    In this file I have written a program to perform different set operation: union, intersection, difference, symmetric difference

---
+++ Setprogram.py
@@ -0,0 +1,10 @@
+E={8,2,6,4,0}
+N={3,2,1,4,5}
+a=E.union(N)
+print("Union of E and N is ",a)
+b=E.intersection(N)
+print("Intersection of E and N is ",b)
+c=E.difference(N)
+print("Difference of E and N is ",c)
+d=E.symmetric_difference(N)
+print("Symmetric difference of E and N is ",d)
